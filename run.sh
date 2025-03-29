76720c73b87072f73f84a1c81891a3755809094e
lucas56098 <lucas@dr-schleuss.de>
2025-03-29 22:12:19 +0100

    .

--- run.sh
+++ run.sh
@@ -2,28 +2,28 @@
 
 # hydro variables ------
 # file naming
-export HYDRO_FOLDER="file_load" 
+export HYDRO_FOLDER="testfolder" 
 export HYDRO_TXT_ADDON="test"
 
 # mesh options
-export HYDRO_N_ROW=72       # cells in x direction (for voronoi -> N = N_ROW**2, for cartesian N_x = N_row, N_y = N_row * L_y/L_x)
-export HYDRO_CARTESIAN=1    # true or false
+export HYDRO_N_ROW=10       # cells in x direction (for voronoi -> N = N_ROW**2, for cartesian N_x = N_row, N_y = N_row * L_y/L_x)
+export HYDRO_CARTESIAN=0    # true or false
 export HYDRO_IS_1D=0        # true or false
 export HYDRO_STRUCTURE=0    # true or false
 export HYDRO_LLOYD_STEPS=0 
-export HYDRO_BOX_L_X=0.5
-export HYDRO_BOX_L_Y=1.5
+export HYDRO_BOX_L_X=1
+export HYDRO_BOX_L_Y=1
 
 # boundary cond
 export HYDRO_BOUNDARY_COND=-1   # -1 : reflective, 1 : zero gradient
 export HYDRO_IS_REPEATING=0     # true or false
 
 # simulation specific
-export HYDRO_TOTAL_SIM_TIME=15
+export HYDRO_TOTAL_SIM_TIME=1
 export HYDRO_CFL=0.1
-export HYDRO_TOTAL_SNAPSHOTS=60
+export HYDRO_TOTAL_SNAPSHOTS=5
 export HYDRO_SIM_ORDER=2            # 1st or 2nd
-export HYDRO_NUM_THREADS=8       # omp thread number
+export HYDRO_NUM_THREADS=4       # omp thread number
 
 # initial conditions
 # 0 : ShockTube
@@ -34,7 +34,7 @@ export HYDRO_NUM_THREADS=8       # omp thread number
 # 5 : const_flow
 # 6 : load from file (also specify directory -> HYDOR_IC_FILE)
 #      ! only FV + Voronoi --- overwrites ALL other specified mesh mesh options --- !
-export HYDRO_IC_VALUE=2
+export HYDRO_IC_VALUE=0
 export HYDRO_IC_FILE="src/files/file_load/v_n30_FV2_BC1_1_5s_test_2_step0.csv"
 
 # ------- Optional: -build to compile hydro before running it -------
